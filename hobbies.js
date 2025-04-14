96ccf91bd6bdf4c000b599fb8a0fed607530d654
simonelupone <simone.lupone@gmail.com>
2025-04-14 15:25:45 +0200

    docs: add jsdocs to modules functions

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,11 @@
+/**
+ * function to generate an array of strings with hobbies
+ * 
+ * @param {string} hobbyOne - hobby one
+ * @param {string} hobbyTwo - hobby two
+ * @param {string} hobbyThree - hobby three
+ * @returns {string[]} - array of hobbies strings
+ */
 const hobbiesArray = (hobbyOne, hobbyTwo, hobbyThree) => [hobbyOne, hobbyTwo, hobbyThree];
 // console.log(hobbiesArray('dormire', 'mangiare', 'giocare'));
 
--- names.js
+++ names.js
@@ -1,3 +1,10 @@
+/**
+ * function to generate an object with firstName and lastName parameters
+ * 
+ * @param {string} firstName - person name
+ * @param {string} lastName - person surname
+ * @returns {{firstName: string, lastName: string}} - object with fist name and last name
+ */
 const namesObj = (firstName, lastName) => ({ firstName, lastName });
 // console.log(namesObj('simone', 'lupone'));
 
