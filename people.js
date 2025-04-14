f25333fb3f3ef8d1b412a41580b758b5db6b878d
simonelupone <simone.lupone@gmail.com>
2025-04-14 15:47:51 +0200

    fix: delete string version

--- people.js
+++ people.js
@@ -1,10 +1,5 @@
 const names = require('./names.js');
 const hobbies = require('./hobbies.js');
 
-// string version
-// const personInfo = names('simone', 'lupone');
-// const personHobbies = hobbies('dormire', 'mangiare', 'giocare');
-// const newObject = () => ({ fullName: personInfo.firstName + ' ' + personInfo.lastName, hobbies: personHobbies.join(', ') });
-
 const newObject = () => ({ fullName: names('simone', 'lupone'), hobbies: hobbies('dormire', 'mangiare', 'giocare') })
 console.log(newObject());
