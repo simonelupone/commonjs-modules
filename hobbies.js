d4d311c125239444ce14a072d966f5b0949f78b6
simonelupone <simone.lupone@gmail.com>
2025-04-14 14:27:43 +0200

    feat: add module with function to generate string array

---
+++ hobbies.js
@@ -0,0 +1,4 @@
+const hobbiesArray = (hobbyOne, hobbyTwo, hobbyThree) => [hobbyOne, hobbyTwo, hobbyThree];
+// console.log(hobbiesArray('dormire', 'mangiare', 'giocare'));
+
+module.exports = hobbiesArray;
