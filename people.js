1233a55a4a63aef61cf2cc5bfc9eb54c3b59a18d
simonelupone <simone.lupone@gmail.com>
2025-04-14 15:28:44 +0200

    feat: import module functions add function to return the final object (two versions)

--- people.js
+++ people.js
@@ -1,1 +1,10 @@
+const names = require('./names.js');
+const hobbies = require('./hobbies.js');
 
+// string version
+// const personInfo = names('simone', 'lupone');
+// const personHobbies = hobbies('dormire', 'mangiare', 'giocare');
+// const newObject = () => ({ fullName: personInfo.firstName + ' ' + personInfo.lastName, hobbies: personHobbies.join(', ') });
+
+const newObject = () => ({ fullName: names('simone', 'lupone'), hobbies: hobbies('dormire', 'mangiare', 'giocare') })
+console.log(newObject());
